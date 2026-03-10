4f984946554130a8e41e6fdf72236a730820d080
Omar Mabrouk <omersa2002@gmail.com>
2026-03-10 04:56:40 +0200

    test middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  RequestMethod,
+  MiddlewareConsumer,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './app/user/user.module';
+import { LoggerMiddleware } from './app/middleware/logger/logger.middleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'user', method: RequestMethod.GET });
+  }
+}
